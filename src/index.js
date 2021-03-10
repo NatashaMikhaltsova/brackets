42a81083890d5e2c7fad175cda85c2a480a5d14e
NatashaMikhaltsova <natashamikhaltsova@gmail.com>
2021-03-10 18:18:33 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openingBrackets = [];
+    let closingBrackets = [];
+    let brackets = [];
+    let current_bracket;
+    let openingSymbol;
+
+    bracketsConfig.forEach((el) => {
+        el.forEach((e, index) => {
+            return index === 0
+                ? openingBrackets.push(e)
+                : closingBrackets.push(e);
+        });
+    });
+
+    for (let i = 0; i < str.length; i++) {
+        //check that symbol is opening tag
+        if (openingBrackets.includes(str[i])) {
+            //Check that symbol is special or not.
+            //If it's special, it will be opening, if the symbol is not in brackets array
+            openingSymbol =
+                openingBrackets.indexOf(str[i]) !==
+                closingBrackets.indexOf(str[i])
+                    ? true
+                    : !brackets.includes(str[i]);
+        } else {
+            openingSymbol = false;
+        }
+
+        if (openingSymbol) {
+            brackets.push(str[i]);
+        } else {
+            if (brackets.length === 0) {
+                return false;
+            }
+
+            current_bracket = brackets.pop();
+            if (
+                openingBrackets.indexOf(current_bracket) !==
+                closingBrackets.indexOf(str[i])
+            ) {
+                return false;
+            }
+        }
+    }
+    return brackets.length > 0 ? false : true;
+};
